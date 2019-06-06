60ca01ce3ade7491c47eeaa159638df0cb23588c
Renan Marques <renan.maarques2@gmail.com>
2019-06-06 17:52:47 -0300

    ajuste banco de dados consign

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql');
 
-module.exports = function () {
+var connMySQL = function () {
     return connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '1234',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function () {
+    return connMySQL;
 }
